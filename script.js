c1600ba3f8908213ab09667a822a2ff6d31c20ef
Bidex-03 <abdullahqaasim14@gmail.com>
2022-09-20 15:58:00 +0100

    changes

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const form = document.querySelector('.subs-form');
 const validation = document.querySelector('.validation');
+const wrapper = document.querySelector('#popup-wrapper');
+const backBtn = document.querySelector('.back-btn');
+const closeBtn = document.querySelector('.close-btn');
 const emailRegex = /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', e => {
     e.preventDefault();
 
     const user = form.email.value;
@@ -11,8 +14,18 @@ form.addEventListener('submit', (e) => {
 
     if (emailRegex.test(user)) {
         console.log('Valid');
+        wrapper.style.display = 'block';
+
     } else {
         console.log('invalid');
         validation.textContent = 'Please provide a valid email address';
     }
+});
+
+backBtn.addEventListener('click', () => {
+    wrapper.style.display = 'none';
+});
+
+closeBtn.addEventListener('click', () => {
+    wrapper.style.display = 'none';
 });
